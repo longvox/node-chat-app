45c1ddf81cea9ca03716a0d0d1eadad5bd6cfce8
imlong <imlong@pop-os.localdomain>
2019-06-27 02:50:45 +0700

    message basic

--- server/server.js
+++ server/server.js
@@ -1,13 +1,48 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+io.on('connection', socket => {
+  console.log('New user connected');
+
+  socket.emit(
+    'newMessage',
+    generateMessage('Thằng mặt lầy', 'Chào mừng tới nơi biệt lập xã hội')
+  );
+
+  socket.broadcast.emit(
+    'newMessage',
+    generateMessage('Thằng mặt lầy', 'Thêm một người nhập hội')
+  );
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
+
+  socket.on('createLocationMessage', coords => {
+    io.emit(
+      'newLocationMessage',
+      generateLocationMessage('Admin', coords.latitude, coords.longitude)
+    );
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
